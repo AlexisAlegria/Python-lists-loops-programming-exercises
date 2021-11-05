26a4f0023ff1bf7662f2696f17eea2722b98a1c4
AlexisAlegria <alexisalegriav@gmail.com>
2021-11-05 01:00:02 +0000

    up to exerise 9

--- exercises/09-Max_integer_from_list/app.py
+++ exercises/09-Max_integer_from_list/app.py
@@ -1,3 +1,12 @@
 my_list = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34]
 
 #Your code go from here:
+
+def maxNumber(items):
+    aux = 0
+    for x in items:
+        if x > aux:
+            aux = x
+    return aux
+
+print(maxNumber(my_list))
