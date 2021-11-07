e029b35f60f2ea9b6b24a8cf2243c955c9eb438c
AlexisAlegria <alexisalegriav@gmail.com>
2021-11-07 18:33:22 +0000

    Finished

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,18 @@
+def lyrics_generator(sequence):
+    output = []
+    counter = 0
 
+    for i in sequence:
+        if i == 0:
+            output.append("Boom")
+        elif i == 1:
+            output.append("Drop the base")
+            counter = counter + 1
+            if counter == 3:
+              output.append("!!!Break the base!!!")
+    final_output = " ".join(output)
+    return final_output
+    
 
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
